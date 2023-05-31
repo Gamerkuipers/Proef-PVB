b81e1d7e82331a98eaa0adb0ff3c850b9cc9ac7e
gamerkuipers <gamerkuipers@gmail.com>
2023-05-31 11:15:39 +0200

    updated to cap

--- app/View/Components/Section/ContactDetails.php
+++ app/View/Components/Section/ContactDetails.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\View\Component;
 
-class contactDetails extends Component
+class ContactDetails extends Component
 {
     public Collection $contactDetails;
 
