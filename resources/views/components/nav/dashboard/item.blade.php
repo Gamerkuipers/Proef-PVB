9f8090b7b46b1c4bce63976e2f1c9fd4d8877090
gamerkuipers <gamerkuipers@gmail.com>
2023-06-01 11:28:29 +0200

    fixed highlighting current nav

--- resources/views/components/nav/dashboard/item.blade.php
+++ resources/views/components/nav/dashboard/item.blade.php
@@ -1,7 +1,7 @@
 @props([
     'route' => null,
     'href' => isset($route) ? route($route) : '',
-    'active' => isset($route) && request()->routeIs(preg_replace('/\.[A-z0-9]*^/', '.*', $route)),
+    'active' => isset($route) && request()->routeIs(preg_replace('/\.[A-z0-9]*$/', '.*', $route)),
 ])
 <a {{ $attributes->class([
     'text-gray-900/40 w-fit hover:text-primary cursor-pointer flex items-center gap-x-1 p-2',
